3d834dfa7250833fd69372837c2110e974e04f36
DavePearce <david.pearce@ecs.vuw.ac.nz>
2014-04-09 16:40:32 +1200

    Added initial plugin activator.

---
+++ src/jasm/Activator.java
@@ -0,0 +1,26 @@
+package jasm;
+
+import wycc.lang.PluginActivator;
+import wycc.lang.PluginContext;
+
+/**
+ * Entry point for Jasm plugin when used within the Whiley Compiler Collection.
+ * 
+ * @author David J. Pearce
+ * 
+ */
+public class Activator implements PluginActivator {
+
+	@Override
+	public void start(PluginContext context) {
+		// TODO Auto-generated method stub
+		System.out.println("JASM PLUGIN ACTIVATED");
+	}
+
+	@Override
+	public void stop(PluginContext context) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
