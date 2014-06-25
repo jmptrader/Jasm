b667369ba3f6ba66fbbd2c9212e92cbf11505ccb
DavePearce <david.pearce@ecs.vuw.ac.nz>
2014-06-25 18:02:06 +1200

    Updated the activator to add appropriate extension points.

--- src/jasm/Activator.java
+++ src/jasm/Activator.java
@@ -3,6 +3,8 @@ package jasm;
 import jasm.lang.ClassFile;
 import jplug.lang.PluginActivator;
 import jplug.lang.PluginContext;
+import wybs.lang.BuildProject;
+import wybs.lang.BuildTask;
 
 /**
  * Entry point for Jasm plugin when used within the Whiley Compiler Collection.
@@ -14,6 +16,9 @@ public class Activator implements PluginActivator {
 
 	@Override
 	public void start(PluginContext context) {
+		// =================================================================
+		// Register Content.Type for class files.
+		// =================================================================	
 		context.register("wyfs.ContentType", new PluginContext.Extension() {
 
 			@Override
@@ -26,6 +31,60 @@ public class Activator implements PluginActivator {
 				return ClassFile.ContentType;
 			}
 			
+		});
+		
+		// =================================================================
+		// Register build tasks for compiling from jasm to class files.
+		// =================================================================
+		context.register("wybs.BuildTask", new PluginContext.Extension() { 
+			@Override
+			public String id() {
+				return "jasm.util.BuildTask";
+			}
+
+			@Override
+			public Object data() {
+				return new BuildTask() {
+
+					@Override
+					public String id() {
+						return "jasmc";
+					}
+
+					@Override
+					public Instance instantiate(BuildProject project) {
+						return null;
+					}
+					
+				};
+			}
+		});
+		
+		// =================================================================
+		// Register build tasks for decompiling from class to jasm files.
+		// =================================================================
+		context.register("wybs.BuildTask", new PluginContext.Extension() { 
+			@Override
+			public String id() {
+				return "jasm.util.BuildTask";
+			}
+
+			@Override
+			public Object data() {
+				return new BuildTask() {
+
+					@Override
+					public String id() {
+						return "jasmd";
+					}
+
+					@Override
+					public Instance instantiate(BuildProject project) {
+						return null;
+					}
+					
+				};
+			}
 		});
 	}
 
